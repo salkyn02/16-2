41513ddeb374b931ff4f70d80953bbfb302c9449
Salkyn Kaiypova <salkynkaiypova@Salkyns-MacBook-Air.local>
2022-07-07 20:16:07 +0400

    Lesson1

---
+++ lesson_1/main.js
@@ -0,0 +1,72 @@
+// const a = "a";
+//
+// console.log(a)
+//
+// let b = 45;
+//
+// console.log(b)
+//
+// b = "change"
+//
+// console.log(b)
+
+const a = "a"; // string
+
+let b = 45; // number
+
+const bool = true; // boolean
+const bool2 = false;
+
+const obj = {
+    key: 'value',
+    username: 'Sara',
+    data: {
+        first_name: "Jack",
+        last_name: "Barbaro"
+    }
+}
+
+// console.log(obj.data.last_name)
+
+const arr = ["aydin", true, false, 35]
+// console.log(arr[1])
+
+const arr2 = [
+    {
+        key: 'value'
+    },
+    {
+        key: 'value2'
+    },
+    {
+        key: 'value3'
+    },
+]
+console.log(arr2[2].key)
+
+// console.log(typeof obj)
+//
+// const name = prompt("What is your name?")
+// console.log(name)
+
+// const age = alert("You are not 18")
+
+const first_number = prompt("Enter first number")
+const last_number = prompt("Enter second number")
+
+// if(first_number > last_number){
+//     console.log(first_number + ">" + last_number)
+// } else if (last_number > first_number){
+//     console.log(first_number + "<" + last_number)
+// } else {
+//     console.log(first_number + "=" + last_number)
+// }
+// const age = prompt("How old are you?")
+//
+// if(age < 18){
+//     console.log("Forbidden to enter")
+// } else if(age > 18){
+//     console.log("You may")
+// } else {
+//     console.log("Wait for one more year")
+// }
