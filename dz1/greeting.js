7e98cd7fcddfee57e98729f851289cf7b201026e
salkyn02 <skaiypova02@icloud.com>
2022-07-10 20:36:43 +0400

    Lesson2

---
+++ dz1/greeting.js
@@ -0,0 +1,11 @@
+const name = prompt("What is your name?")
+console.log(name)
+
+const user = alert("Hello," + name)
+
+
+let arrOne = ['Ivan', 'Olya', 'Kot']
+console.log(arrOne)
+console.log(arrOne.length)
+
+let arrTwo = ['Emil', true, false, 35]
